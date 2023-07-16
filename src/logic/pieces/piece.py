a06481ef76e433f77a970bb83f913d6046f25adf
Glitchez <87347098+Glitchez-1984@users.noreply.github.com>
2023-07-16 13:36:03 +0300

    Update piece.py

--- src/logic/pieces/piece.py
+++ src/logic/pieces/piece.py
@@ -1,7 +1,8 @@
 class Piece:
-    def __init__(self, white: bool, pos: tuple, type: int):
+    def __init__(self, white: bool, pos: tuple, type: int, has_moved: bool):
         self.white = white
         self.pos = pos
         self.type = type
+        self.has_moved = has_moved
 
 
